fbac3207959c680233f2137f27241dae9fd43c9d
Innoecho <774665816@qq.com>
2016-03-10 14:42:06 +0800

    add callback

--- APMC/Callback.py
+++ APMC/Callback.py
@@ -1,14 +1,65 @@
-
+#http://python.dronekit.io/guide/vehicle_state_and_parameters.html#vehicle-state-observe-attributes
 
 class Callback:
     def __init__(self):
         pass
     """Functions for attributes callback"""
+    def cb_version(self, vehicle, attr, value):
+        print "Attribute Update - Version: %s" % value
+
+    def cb_capabilities(self, vehicle, attr, value):
+        print "Attribute Update - Capabilities: %s" % value
+
     def cb_global_frame(self, vehicle, attr, value):
-        print "Attribute Update - Global Location: %s" % value
+        print "Attribute Update - %s" % value
+
+    def cb_global_relative_frame(self, vehicle, attr, value):
+        print "Attribute Update - %s" % value
+
+    def cb_local_frame(self, vehicle, attr, value):
+        print "Attribute Update - Local Location: %s" % value
+
+    def cb_attitude(self, vehicle, attr, value):
+        print "Attribute Update - %s" % value
+
+    def cb_velocity(self, vehicle, attr, value):
+        print "Attribute Update - Velocity: %s" % value
+
+    def cb_gps_0(self, vehicle, attr, value):
+        print "Attribute Update - %s" % value
+
+    def cb_gimbal(self, vehicle, attr, value):
+        print "Attribute Update - Gimbal: %s" % value
+
+    def cb_battery(self, vehicle, attr, value):
+        print "Attribute Update - %s" % value
+
+    def cb_rangerfinder(self, vehicle, attr, value):
+        print "Attribute Update - RangerFinder: %s" % value
+
+    def cb_ekf_ok(self, vehicle, attr, value):
+        print "Attribute Update - EKF_OK: %s" % value
+
+    def cb_last_heartbeat(self, vehicle, attr, value):
+        print "Attribute Update - Last Heartbeat: %s" % value
+
+    def cb_home_location(self, vehicle, attr, value):
+        print "Attribute Update - Home Location: %s" % value
 
     def cb_system_status(self, vehicle, attr, value):
         print "Attribute Update - System status: %s" % value
+
+    def cb_heading(self, vehicle, attr, value):
+        print "Attribute Update - Heading: %s" % value
+
+    def cb_is_armable(self, vehicle, attr, value):
+        print "Attribute Update - Is Armable: %s" % value
+
+    def cb_airspeed(self, vehicle, attr, value):
+        print "Attribute Update - Airspeed: %s" % value
+
+    def cb_groundspeed(self, vehicle, attr, value):
+        print "Attribute Update - Groundspeed: %s" % value
 
     def cb_armed(self, vehicle, attr, value):
         print "Attribute Update - Armed: %s" % value
--- test.py
+++ test.py
@@ -2,13 +2,13 @@ from APMC.APMConnector import APMConnector
 
 
 def main():
-    vehicle = APMConnector('/dev/ttyUSB0',57600)
+    vehicle = APMConnector('tcp:127.0.0.1:5760',57600)
     #vehicle.connect()
     vehicle.getAttributes()
     vehicle.addListeners()
     vehicle.armed()
     #vehicle.getHomeLocation()
-    vehicle.gerParams()
+    #vehicle.gerParams()
     vehicle.armAndTakeOff(1)
     #vehicle.sendNedVelocity(1,0,0,2)
 
